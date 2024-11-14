b00a21f156af17b9204692e66c23f97e6f72278e
Edward James Wells <edwardjameswells@gmail.com>
2024-11-14 18:52:19 +0000

    feat: Add code to process the conjugation data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+import * as conjugationDataSets from './data.js';
+
 document.addEventListener('DOMContentLoaded', () => {
     const dataSet = document.getElementById('data-set');
     const questionForms = document.getElementById('question-form');
@@ -32,3 +34,57 @@ document.addEventListener('DOMContentLoaded', () => {
     // run on page load rather than waiting to run manually
     run();
 });
+
+/**
+ * Returns an array of the names of the data sets.
+ * @returns {array}
+ */
+function getDataSets() {
+    let dataSets = new Array();
+    for (const dataSet in conjugationDataSets) {
+        dataSets.push(dataSet);
+    }
+
+    return dataSets;
+}
+
+/**
+ * Creates the HTML option elements as children of the select element for
+ * choosing between the different conjugation data sets.
+ */
+function buildDataMenu() {
+    const dataSets = getDataSets();
+    const dataSelect = document.getElementById('data-set');
+
+    for (const dataSet of dataSets) {
+        let opt = document.createElement('option');
+        opt.textContent = displayFormat(dataSet);
+        opt.setAttribute('value', dataSet);
+        dataSelect.appendChild(opt);
+    }
+    document.querySelector('option:last-of-type').selected = true;
+    dataSelect.setAttribute('data-choice', dataSelect.value);
+}
+
+/**
+ * Returns a titlecase string more appropriate (nicer) for textual display
+ * @param {string} str - the string to format
+ * @returns {string} The ‘display’ titlecase version of the input string
+ */
+function displayFormat(str) {
+    const format = str.replace(/([a-z])([A-Z])/g, '$1 $2');
+
+    return format.charAt(0).toUpperCase() + format.slice(1);
+}
+
+function buildConjugationMenus() {
+    //
+}
+
+function run() {
+    //
+}
+
+function checkAnswer() {
+    //
+}
